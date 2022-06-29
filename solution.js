32794b1b175d110ce3afb033790c352e9239f7b9
harshita <harshita1225@gmail.com>
2022-06-29 12:39:04 +0200

    completed all tasks, created info profile

--- solution.js
+++ solution.js
@@ -1,1 +1,35 @@
 // write your code in this file
+
+let text ='"This is an exercise in console logging"';
+console.log(text);
+
+
+const exercise ='This is the value';
+console.log(exercise);     
+
+
+const firstName = 'Harshita';
+const lastName = 'Joshi';
+console.log( 'My name is ',firstName,lastName);
+
+const myProfile = {
+city:"Berlin",
+age: 30,
+job:"Student",
+};
+
+console.log(firstName,lastName,myProfile);
+
+const personInfo =["Name-Harshita","Surname-Joshi", "age30", "dob-25 Dec", "city-Berlin"];
+console.log(personInfo);
+
+
+console.log('My name is '+firstName,'My last name is '+lastName,'i live in '+myProfile.city,'i am',+myProfile.age,'years old.','i am a ', +myProfile.job,'by profession');
+
+console.log( personInfo[0], personInfo[1], 'i live in',personInfo[4]);
+
+
+
+
+                                         
+                                                                                                                                                        
