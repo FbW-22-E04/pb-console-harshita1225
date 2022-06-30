7826e8ef2f4a47ca36c1ea98311c6f6ff5b16ad0
harshita <harshita1225@gmail.com>
2022-06-30 09:37:14 +0200

    edited

--- solution.js
+++ solution.js
@@ -1,34 +1,36 @@
 // write your code in this file
 
-let text ='"This is an exercise in console logging"';
-console.log(text);
+console.log("This is an exercise in console logging");
 
 
-const exercise ='This is the value';
+const exercise ="This is the value";
 console.log(exercise);     
 
 
-const firstName = 'Harshita';
-const lastName = 'Joshi';
+const firstName = "Harshita";
+const lastName = "Joshi";
+const age = 30;
+const city ="Berlin";
+const job = "Student";
 console.log( 'My name is ',firstName,lastName);
 
-const myProfile = {
+/*const myProfile = {
 city:"Berlin",
 age: 30,
 job:"Student",
-};
+};*/
 
-console.log(firstName,lastName,myProfile);
+console.log(firstName,lastName, age, city, job);
 
-const personInfo =["Name-Harshita","Surname-Joshi", "age30", "dob-25 Dec", "city-Berlin"];
+/*const personInfo =["Name-Harshita","Surname-Joshi", "age30", "dob-25 Dec", "city-Berlin"];
 console.log(personInfo);
+*/
 
+/*console.log('My name is '+firstName,'My last name is '+lastName,'i live in '+myProfile.city,'i am',myProfile.age,'years old.','i works as ', myProfile.job,'by profession');
 
-console.log('My name is '+firstName,'My last name is '+lastName,'i live in '+myProfile.city,'i am',myProfile.age,'years old.','i works as ', myProfile.job,'by profession');
+console.log( personInfo[0], personInfo[1], 'i live in',personInfo[4]);*/
 
-console.log( personInfo[0], personInfo[1], 'i live in',personInfo[4]);
-
-
+console.log(firstName,lastName, 'is a' , age, 'years old',job, 'who lives in' , city);
 
 
                                          
